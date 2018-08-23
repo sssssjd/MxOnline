213aa8bc97d08dfc0e09c5fbfb5b1b3eac576888
sssssjd <sssssjd@163.com>
2018-08-23 19:10:23 +0800

    Initial commit

---
+++ apps/organization/adminx.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+__author__ = 'sssssjd'
+__date__ = '2018/8/22 19:10'
+
+import xadmin
+from .models import CityDict, CourseOrg, Teacher
+
+
+class CityDictAdmin(object):
+    list_display = ['name', 'desc', 'add_time']
+    search_fields = ['name', 'desc']
+    list_filter = ['name', 'desc', 'add_time']
+
+
+class CourseOrgAdmin(object):
+    list_display = ['name', 'desc', 'click_nums', 'fav_nums', 'image', 'address',
+                    'city', 'add_time']
+    search_fields = ['name', 'desc', 'click_nums', 'fav_nums', 'address', 'city']
+    list_filter = ['name', 'desc', 'click_nums', 'fav_nums', 'address',
+                   'city__name', 'add_time']
+
+
+class TeacherAdmin(object):
+    list_display = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points',
+                    'click_nums', 'fav_nums', 'add_time']
+    search_fields = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points',
+                     'click_nums', 'fav_nums']
+    list_filter = ['org__name', 'name', 'work_years', 'work_company', 'work_position', 'points',
+                   'click_nums', 'fav_nums', 'add_time']
+
+
+xadmin.site.register(CityDict, CityDictAdmin)
+xadmin.site.register(CourseOrg, CourseOrgAdmin)
+xadmin.site.register(Teacher, TeacherAdmin)
+
---
+++ apps/users/forms.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+__author__ = 'sssssjd'
+__date__ = '2018/8/23 16:28'
+
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, min_length=5)
