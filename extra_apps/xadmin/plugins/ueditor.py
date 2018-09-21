c54a309e68012b36e87af3cf6036f638cbb69b9e
sssssjd <sssssjd@163.com>
2018-09-21 20:10:56 +0800

    集成了ueditor，存在问题

--- extra_apps/xadmin/plugins/ueditor.py
+++ extra_apps/xadmin/plugins/ueditor.py
@@ -2,20 +2,18 @@
 __author__ = 'sssssjd'
 __date__ = '2018/9/21 10:10'
 
-
 import xadmin
-from DjangoUeditor.widgets import UEditorWidget
-from django.db.models import TextField
-from xadmin.views import BaseAdminPlugin, CreateAdminView, UpdateAdminView
+from xadmin.views import BaseAdminPlugin, CreateAdminView, ModelFormAdminView, UpdateAdminView
 from DjangoUeditor.models import UEditorField
+from DjangoUeditor.widgets import UEditorWidget
 from django.conf import settings
 
 
 class XadminUEditorWidget(UEditorWidget):
-    def __init__(self, **kwargs):
-        self.ueditor_options = kwargs
+    def __init__(self,**kwargs):
+        self.ueditor_options=kwargs
         self.Media.js = None
-        super(XadminUEditorWidget, self).__init__(kwargs)
+        super(XadminUEditorWidget,self).__init__(kwargs)
 
 
 class UeditorPlugin(BaseAdminPlugin):
@@ -27,12 +25,12 @@ class UeditorPlugin(BaseAdminPlugin):
                 param = {}
                 param.update(widget.ueditor_settings)
                 param.update(widget.attrs)
-                return {'widget', XadminUEditorWidget(**param)}
+                return {'widget': XadminUEditorWidget(**param)}
         return attrs
 
     def block_extrahead(self, context, nodes):
-        js = '<script type="text/javascript" src="%s"></script>' % (settings.STATIC_URL + 'ueditor/ueditor.config.js')
-        js += '<script type="text/javascript" src="%s"></script>' % (settings.STATIC_URL + 'ueditor/ueditor.all.min.js')
+        js = '<script type="text/javascript" src="%s"></script>' % (settings.STATIC_URL + "ueditor/ueditor.config.js")         #自己的静态目录
+        js += '<script type="text/javascript" src="%s"></script>' % (settings.STATIC_URL + "ueditor/ueditor.all.min.js")   #自己的静态目录
         nodes.append(js)
 
 
